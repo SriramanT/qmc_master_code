dbaf4d11ce57bc3dd20fa51c92a28fbde6854844
Francisco Brito <franciscobrito@Franciscos-MBP.lan>
2018-04-23 11:38:30 +0100

    to figure out updates and wrapping

--- hubbard1D/afqmcHubbardOneDimension.cpp
+++ hubbard1D/afqmcHubbardOneDimension.cpp
@@ -22,72 +22,24 @@
 #include <unsupported/Eigen/MatrixFunctions>
 #include <fstream>
 
-#define NSITES 100
+#define NSITES 10
 
 using namespace Eigen;
 using namespace std;
 
-// Auxiliary functions
+// Auxiliary function declarations
 
-MatrixXd createHoppingMatrix(int nSites)
-{
-    MatrixXd K = MatrixXd::Zero(NSITES, NSITES) ;
-    // Set the elements of the hopping matrix that define PBC corresponding to the ends of the 1D chain
-    K(0, 1) = 1;
-    K(0, nSites - 1) = 1;
-    K(nSites - 1, 0) = 1;
-    K(nSites - 1, nSites - 2) = 1;
-    
-    // Set the remaining ones
-    int i;
-    for (i = 1; i < nSites - 1; i++)
-    {
-        K(i, i - 1) = 1;
-        K(i, i + 1) = 1;
-    }
-    
-    return K;
-}
-
-MatrixXd generateHSmatrix(int L, int nSites)
-{
-    // Generate the HS field matrix
-
-    int l;
-    int i;
-    
-    MatrixXd h = MatrixXd::Random(L,nSites);
-    
-    for (l = 0; l < L; l++)
-    {
-      
-      for (i = 0; i < NSITES; i++)
-      {
-          if ( h(l, i) < 0 )
-          {
-              h(l, i) = -1;
-          }
-          else
-          {
-              h(l, i) = 1;
-          }
-      }
-    }
-    
-    return h;
-}
+MatrixXd createHoppingMatrix(int nSites);
+MatrixXd generateHSmatrix(int L, int nSites);
     
 int main()
 {
-    // Toggle prints
-    
-    int printsOn = 0;                                               // 0 - NO PRINTS, 1 - PRINTS
-
     cout << "\nAuxiliary field QMC for 1D Hubbard chain\n" << endl;
     cout << "\nNumber of sites: " << NSITES << endl;
     
-    //  Usual general use variables
+    // --- DEFINITIONS ---
     
+    //  Some general use variables
     int i;
     int j;
     int l;
@@ -95,47 +47,50 @@ int main()
     int step;
     int i_chosen;
     int l_chosen;
+    
+    //  Toggle prints
+    int printsOn = 1;                                               // 0 - NO PRINTS, 1 - PRINTS
 
     //  Things to do with random numbers throughout the code
-    
     const static int seed = 12345;                              //  set a seed
     mt19937 gen(seed);                                          //  mt19937 algorithm to generate random numbers
     uniform_real_distribution<> dis(0.0, 1.0);                  //  set the distribution from which to draw numbers
     double decisionMaker;                                       //  to accept or not to accept, hence the question.
     
-    //  Monte Carlo specific variables
-    
-    const static int totalMCSteps = 100;
+    //  Monte Carlo-specific variables
+    const static int totalMCSteps = 20;
     const static int W = 3000;                                  //  warm-up steps
     const static int autoCorrTime = 500;                        //  auto-correlation time
     const static int M = (totalMCSteps - W)/autoCorrTime;       //  number of measurements
     
     //  Physical parameters
-    
-    const static int L = 5;                                     //  number of imaginary time subintervals
-    cout << "L: " << L << endl;
-    const static double beta = .1;                              //  imaginary time interval or equivalent maximum temperature of the (d+1) classical system
-    cout << "beta: " << beta << endl;
-    const static double dt = beta/L;                            //  time subinterval width
+    const static double dt = 0.2;                               //  time subinterval width. error scales as dt^2
     cout << "dt: " << dt << endl;
+    const static double beta = 1.;                              //  imaginary time interval or equivalent maximum temperature of the (d+1) classical system
+    cout << "beta: " << beta << endl;
+    const static int L = beta/dt;                               //  number of imaginary time subintervals
+    cout << "L: " << L << endl;
     const static double t = 1.;                                 //  hopping parameters
     cout << "t: " << t << endl;
-    const static double U = 5;                                //  interaction energy
+    const static double U = 5;                                  //  interaction energy
     cout << "U: " << U << endl;
     double nu = acosh( exp( U * dt / 2 ) );                     //  Hubbard Stratonovich transformation parameter
     
-    // Initialize the HS field at all time slices in [0, beta] with +1 and -1 randomly
+    cout << "Number of MC sweeps: " << totalMCSteps / (NSITES*L) << " (" << totalMCSteps << " steps)" << "\n\n";
     
+    
+    // --- INITIALIZATION ---
+    
+    
+    // Initialize the HS field at all time slices in [0, beta] with +1 and -1 randomly
     MatrixXd h;
     h = generateHSmatrix(L, NSITES); // HS field h_{l = 1,... L, i = 1, ..., NSITES}
     
     // Hopping matrix 1D chain w/ PBC
-    
     MatrixXd K;
     K = createHoppingMatrix(NSITES);
     
     // Compute matrix 'prefactor' of the B-matrices e^{t\delta\tau K}
-    
     const static MatrixXd B_preFactor = (t * dt * K).exp();
   
     // Some prints for debugging. Toggle them if you want at the top.
@@ -147,7 +102,6 @@ int main()
     }
     
     // Build the B-matrices. We need a copy of each to perform the updates (naively)
-    
     MatrixXd BpOld[L]; // B plus
     MatrixXd BmOld[L]; // B minus
     MatrixXd BpNew[L]; // B plus
@@ -161,18 +115,11 @@ int main()
         BmOld[l] =  MatrixXd::Identity(NSITES,NSITES);
         BmOld[l].diagonal() = (-nu) * h.row(l);
         BmOld[l] = B_preFactor * BmOld[l].exp();
-        // The new matrices are initially set as equal to the old ones
         BpNew[l] = BpOld[l];
         BmNew[l] = BmOld[l];
-//        if (printsOn ==1)
-//        {
-//            cout << "\n\nHere is a B_plus matrix\n\n" << BpOld[l] << endl;
-//            cout << "\n\nHere is a B_minus matrix\n\n" << BmOld[l] << endl;
-//        }
     }
     
     // Build the M-matrices and Green's function (matrix)
-    
     MatrixXd MPlusOld = MatrixXd::Identity(NSITES,NSITES);
     MatrixXd MMinusOld = MatrixXd::Identity(NSITES,NSITES);
     MatrixXd MPlusNew;
@@ -189,20 +136,19 @@ int main()
     
     MatrixXd GreenPlus;
     MatrixXd GreenMinus;
-    
+//    MatrixXd wrGreenPlus;
+//    MatrixXd wrGreenMinus;
     GreenPlus = MPlusOld.inverse();
     GreenMinus = MMinusOld.inverse();
+//    wrGreenPlus = GreenPlus;
+//    wrGreenMinus = GreenMinus;
     
-//    if (printsOn == 1)
-//    {
-//        cout << "\n\nHere is a M_plus matrix\n\n" << MPlusOld << endl;
-//        cout << "\n\nHere is a M_minus matrix\n\n" << MMinusOld << endl;
-//        cout << "\n\nHere is the M_plus determinant\n\n" << MPlusOld.determinant() << endl;
-//        cout << "\n\nHere is the M_minus determinant\n\n" << MMinusOld.determinant() << endl;
-//    }
+//    cout << "\n\nHere is a M_plus matrix\n\n" << MPlusOld << endl;
+//    cout << "\n\nHere is a M_minus matrix\n\n" << MMinusOld << endl;
+//    cout << "\n\nHere is the M_plus determinant\n\n" << MPlusOld.determinant() << endl;
+//    cout << "\n\nHere is the M_minus determinant\n\n" << MMinusOld.determinant() << endl;
     
     //  Initialize determinants and acceptance ratio
-    
     double detOldPlus = MPlusOld.determinant();
     double detOldMinus = MMinusOld.determinant();
     double detsProdOld = detOldPlus * detOldMinus;
@@ -214,142 +160,110 @@ int main()
     double alphaPlus;
     double alphaMinus;
     
-    double weight = detsProdOld;
-    
-    VectorXd weights(totalMCSteps);
+    VectorXd weightsNaive(totalMCSteps);
+    VectorXd weightsUpdate(totalMCSteps);
 //    VectorXd density(M);
+
+    weightsUpdate(0) = detsProdOld;
+    
+    
+    // --- MC LOOP ---
+    
     
     //  inititialize chosen entry of HS field matrix to (0, 0)
-    
     l_chosen = 0;
     i_chosen = 0;
     
     for (step = 0; step < totalMCSteps; step++)
     {
+        //  To check progress of the run
         if ((step+1) % (totalMCSteps/10) == 0)
         {
-            cout << "\nstep: " << step + 1 << "/" << totalMCSteps << endl; // print step
+            cout << "\nMC loop: " << (step + 1)*1. / totalMCSteps * 100 << " %" << endl;
         }
         
-        if (printsOn == 1)
-        {
-            cout << "Chosen l: " << l_chosen << endl;
-            cout << "Chosen i: " << i_chosen << endl;
-        }
-        
-//        //  Measurements
-//
+        //  Measurements
 //        if ((step > W) and (step % autoCorrTime == 0))
 //        {
 //            density(m) = ( ((MPlusNew.inverse()).diagonal()).trace() + ((MMinusNew.inverse()).diagonal()).trace() ) / ( 2 * NSITES);
 //        }
         
         // save weight of the configuration to see convergence
+        weightsNaive(step) = detsProdNew;
         
-        weights(step) = weight;
-        
-        // flip
+        // flip a 'spin'
         h(l_chosen, i_chosen) *= -1;
         
         // rebuild B-matrices
-
         BpNew[l_chosen] =  MatrixXd::Identity(NSITES,NSITES);
-    
         BpNew[l_chosen].diagonal() = nu * h.row(l_chosen);
-        
         BpNew[l_chosen] = B_preFactor * BpNew[l_chosen].exp();
-        
-        if (printsOn == 1)
-        {
-            cout << "\n\nOld B_plus matrix\n\n" << BpOld[l_chosen] << endl;
-            cout << "\n\nNew B_plus matrix\n\n" << BpNew[l_chosen] << endl;
-        }
-
+//        cout << "\n\nOld B_plus matrix\n\n" << BpOld[l_chosen] << endl;
+//        cout << "\n\nNew B_plus matrix\n\n" << BpNew[l_chosen] << endl;
         BmNew[l_chosen] =  MatrixXd::Identity(NSITES,NSITES);
-
         BmNew[l_chosen].diagonal() = (-nu) * h.row(l_chosen);
-
         BmNew[l_chosen] = B_preFactor * BmNew[l_chosen].exp();
-        
-        if (printsOn == 1)
-        {
-            cout << "\n\nOld B_minus matrix\n\n" << BmOld[l_chosen] << endl;
-            cout << "\n\nNew B_minus matrix\n\n" << BmNew[l_chosen] << endl;
-        }
+//        cout << "\n\nOld B_minus matrix\n\n" << BmOld[l_chosen] << endl;
+//        cout << "\n\nNew B_minus matrix\n\n" << BmNew[l_chosen] << endl;
         
         // rebuild M-matrices
-        
         MPlusNew = MatrixXd::Identity(NSITES,NSITES);
         MMinusNew = MatrixXd::Identity(NSITES,NSITES);
-        
         for (l = 0; l < L; l++)
         {
             MPlusNew *= BpNew[L - 1 - l];
             MMinusNew *= BmNew[L - 1 - l];
         }
-
         MPlusNew += MatrixXd::Identity(NSITES,NSITES);
         MMinusNew += MatrixXd::Identity(NSITES,NSITES);
-            
-//        if (printsOn == 1)
-//        {
-//            cout << "\n\nOld M_plus matrix\n\n" << MPlusOld << endl;
-//            cout << "\n\nOld M_minus matrix\n\n" << MMinusOld << endl;
-//            cout << "\n\nNew M_plus matrix\n\n" << MPlusNew << endl;
-//            cout << "\n\nNew M_minus matrix\n\n" << MMinusNew << endl;
-//            cout << "\n\nNew M_plus determinant\n\n" << MPlusNew.determinant() << endl;
-//            cout << "\n\nNew M_minus determinant\n\n" << MMinusNew.determinant() << endl;
-//        }
+//        cout << "\n\nOld M_plus matrix\n\n" << MPlusOld << endl;
+//        cout << "\n\nOld M_minus matrix\n\n" << MMinusOld << endl;
+//        cout << "\n\nNew M_plus matrix\n\n" << MPlusNew << endl;
+//        cout << "\n\nNew M_minus matrix\n\n" << MMinusNew << endl;
+//        cout << "\n\nNew M_plus determinant\n\n" << MPlusNew.determinant() << endl;
+//        cout << "\n\nNew M_minus determinant\n\n" << MMinusNew.determinant() << endl;
 
+        //  Compute the acceptance ratio
         detNewPlus = MPlusNew.determinant();
         detNewMinus = MMinusNew.determinant();
         detsProdNew = detNewPlus * detNewMinus;
         acceptanceRatio = detsProdNew / detsProdOld;
-        
         alphaPlus = ( exp( -2 * (-1) * h(l_chosen,i_chosen) * nu ) - 1 );
         alphaMinus = ( exp( 2 * (-1) * h(l_chosen,i_chosen) * nu ) - 1 );
-        
         smartAccept = ( 1 + alphaPlus  * ( 1 - GreenPlus(i_chosen, i_chosen) ) ) * ( 1 + alphaMinus  * ( 1 - GreenMinus(i_chosen, i_chosen) ) );
+
+        if (step < totalMCSteps - 1)
+        {
+            weightsUpdate(step + 1) = smartAccept * weightsUpdate(step) ;
+        }
         
-        weight *= smartAccept;
-        
-//        cout << "\n\nGreenPlus\n\n" << GreenPlus << endl;
-//        cout << "\n\nGreenMinus\n\n" << GreenMinus << endl;
-//
-//        if (printsOn == 1)
-//        {
-//            cout << "\n\nacceptance ratio: " << acceptanceRatio << endl;
-//        }
-//
-        cout << "\n\nacceptance ratio: " << acceptanceRatio << endl;
-        cout << "\n\nacceptance ratio: " << smartAccept << endl;
-        
+        cout << "\n\nGreenPlus\n\n" << GreenPlus << endl;
+        cout << "\n\nGreenMinus\n\n" << GreenMinus << endl;
+
+        if (printsOn == 1)
+        {
+            cout << "\n\nacceptance ratio: " << acceptanceRatio << " (naive)" << endl;
+            cout << "\n\nacceptance ratio: " << smartAccept << " (via Green's function update)" << endl;
+        }
+
         // draw random number to decide
-        
         decisionMaker = dis(gen);
         
         if (decisionMaker <= acceptanceRatio)
         {
-            //update everything
+            //update everything -> h(l_chosen, i_chosen) = h(l_chosen, i_chosen) (already updated)
             BpOld[l_chosen] = BpNew[l_chosen];
             BmOld[l_chosen] = BmNew[l_chosen];
             MPlusOld = MPlusNew;
             MMinusOld = MMinusNew;
-            //h(l_chosen, i_chosen) = h(l_chosen, i_chosen);
             detsProdOld = detsProdNew;
             
-            if (printsOn == 1)
-            {
-                cout << "Accepted\n" << endl;
-            }
-            
-//            // Brute force update
-//
+            // Brute force update
+
 //            GreenPlus = MPlusNew.inverse();
 //            GreenMinus = MMinusNew.inverse();
-            
-            // Rank-one update --> O ( 2 * N^2 )
 
+            // Rank-one update --> O ( 2 * N^2 )
             for (i = 0; i < NSITES; i++)
             {
                 for (j = 0; j < NSITES; j++)
@@ -359,36 +273,27 @@ int main()
                     {
                         GreenPlus(i, j) *= ( 1 + alphaPlus/smartAccept * ( GreenPlus(i_chosen, i_chosen) - 1 ) );
                         GreenMinus(i, j) *= ( 1 + alphaMinus/smartAccept * ( GreenMinus(i_chosen, i_chosen) - 1 ) );
+
                     }
                     else
                     {
-                        GreenPlus(i, j) += alphaPlus/smartAccept *  GreenPlus(i, i_chosen) * GreenPlus(i_chosen, j);
-                        GreenMinus(i, j) += alphaMinus/smartAccept *  GreenMinus(i, i_chosen) * GreenMinus(i_chosen, j);
+                        GreenPlus(i, j) -= alphaPlus/smartAccept *  GreenPlus(i, i_chosen) * GreenPlus(i_chosen, j);
+                        GreenMinus(i, j) -= alphaMinus/smartAccept *  GreenMinus(i, i_chosen) * GreenMinus(i_chosen, j);
                     }
-                    
-
                 }
             }
-            
-//            cout << "Update\n" << endl;
-//
-//            cout << "\n\nGreenPlus\n\n" << GreenPlus << endl;
-//            cout << "\n\nGreenMinus\n\n" << GreenMinus << endl;
-//
-//            cout << "Brute force\n" << endl;
-//
-//            cout << MPlusNew.inverse() << endl << endl;
-//            cout << MMinusNew.inverse() << endl << endl;
 
+            cout << "\n\nO(N^2) update of the Green's function" << endl;
+            cout << "\n\nGreenPlus\n\n" << GreenPlus << endl;
+            cout << "\n\nGreenMinus\n\n" << GreenMinus << endl;
+            cout << "\n\nBrute force update\n\n" << endl;
+            cout << MPlusNew.inverse() << endl << endl;
+            cout << MMinusNew.inverse() << endl << endl;
             
         }
         else
         {
-            if (printsOn == 1)
-            {
-                cout << "Rejected\n" << endl;
-            }
-            // revert changes (switch new -> old wrt the above commands)
+            // revert changes (switch new -> old wrt the above statements)
             BpNew[l_chosen] = BpOld[l_chosen];
             BmNew[l_chosen] = BmOld[l_chosen];
             MPlusNew = MPlusOld;
@@ -397,23 +302,29 @@ int main()
             detsProdNew = detsProdOld;
         }
         
-        // 'pick' a site (spatial + HS field)
-        
+        //  loop through (l, i)
         if (i_chosen < NSITES - 1)
         {
             i_chosen += 1;
             //l_chosen = l_chosen;
         }
-        
         else
         {
             if (l_chosen < L - 1)
             {
+//                // Wrapping
+//                GreenPlus = BpNew[l_chosen].inverse() * GreenPlus * BpNew[l_chosen];
+//                GreenMinus = BmNew[l_chosen].inverse() * GreenMinus * BmNew[l_chosen];
+
                 l_chosen += 1;
                 i_chosen = 0;
             }
             else
             {
+//                // Back to original order
+//                GreenPlus = BpNew[l_chosen].inverse() * GreenPlus * BpNew[l_chosen];
+//                GreenMinus = BmNew[l_chosen].inverse() * GreenMinus * BmNew[l_chosen];
+                
                 l_chosen = 0;
                 i_chosen = 0;
             }
@@ -427,8 +338,14 @@ int main()
     std::ofstream file("test.txt");
     if (file.is_open())
     {
-        file << weights << '\n';
+        file << weightsNaive << '\n';
     }
+    
+//    std::ofstream file2("test2.txt");
+//    if (file2.is_open())
+//    {
+//        file2 << weightsUpdate << '\n';
+//    }
 
 //    // Save densities to file
 //
@@ -442,3 +359,4 @@ int main()
     
     return 0;
 }
+
---
+++ hubbard1D/generateHSmatrix.cpp
@@ -0,0 +1,37 @@
+//
+//  createHoppingMatrix.cpp
+//  
+//
+//  Created by Francisco Brito on 23/04/2018.
+//
+
+#include <Eigen/Dense>
+using namespace Eigen;
+
+MatrixXd generateHSmatrix(int L, int nSites)
+{
+    // Generate the HS field matrix
+    
+    int l;
+    int i;
+    
+    MatrixXd h = MatrixXd::Random(L,nSites);
+    
+    for (l = 0; l < L; l++)
+    {
+        
+        for (i = 0; i < nSites; i++)
+        {
+            if ( h(l, i) < 0 )
+            {
+                h(l, i) = -1;
+            }
+            else
+            {
+                h(l, i) = 1;
+            }
+        }
+    }
+    
+    return h;
+}
